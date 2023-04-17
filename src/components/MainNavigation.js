908ffb0e448734686ab74afee594ad27a1de1a11
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-04-17 19:44:42 +0530

    develop basic project with components

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import classes from "./MainNavigation.module.css"
+
+const MainNavigation = () => {
+    return(
+        <header className={classes.header}>
+            <nav>
+                <ul className={classes.list}>
+                    <li>
+                        <a>Home</a>
+                    </li>
+                    <li>
+                        <a>Events</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainNavigation

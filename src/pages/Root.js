a8d278aeb136c6562e4b58d8de59970c4a7683bc
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-04-17 22:14:56 +0530

    develop loading state with useNavigation()

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 
 const Root = () => {
+    const navigation = useNavigation()
+
     return(
         <Fragment>
             <MainNavigation />
             <main>
+                {navigation.state === "loading" && <p>Loading...</p>}
                 <Outlet />
             </main>
         </Fragment>
@@ -15,3 +18,12 @@ const Root = () => {
 }
 
 export default Root
+
+/**
+ * useNavigation() -> use to check current route transition state
+ * 
+ * there are three states (loading / idle / submitting)
+ * 
+ * loader indicator should nt be added to page which transitioning to,
+ * it should be added to some page or component which is already visible
+ */

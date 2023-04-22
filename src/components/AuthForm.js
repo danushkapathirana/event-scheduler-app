cabb4fa1cd07694ca3f06713156ad822c1ba96d6
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-04-22 11:35:18 +0530

    develop to show backend validation errors, when try to create a user with existing email

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Form, Link, useSearchParams } from "react-router-dom";
+import { Form, Link, useActionData, useSearchParams } from "react-router-dom";
 
 import classes from "./AuthForm.module.css"
 
 const AuthForm = () => {
+    const data = useActionData()
     // const [searchParams, setSearchParams] = useSearchParams()
     const [searchParams] = useSearchParams()
     const isLogin = searchParams.get("mode") === "login"
@@ -11,6 +12,23 @@ const AuthForm = () => {
     return(
         <Form method="post" className={classes.form}>
             <h1>{isLogin ? "Login" : "Create a new user"}</h1>
+
+            {/* backend validations */}
+            {
+                data && data.errors &&
+                <ul>
+                    {Object.values(data.errors).map((error) => (
+                        <li key={error}>
+                            {error}
+                        </li>
+                    ))}
+                </ul>
+            }
+            {
+                data && data.message &&
+                <p>{data.message}</p>
+            }
+            
             <p>
                 <label htmlFor="email">Email</label>
                 <input id="email" type="email" name="email" required />
